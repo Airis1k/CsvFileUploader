90f08f1ed1365f49c999c02e33ba1d7325b0eaf3
Airis1k <airingasstar@gmail.com>
2023-02-20 10:51:46 +0200

    Pirmas commitas. Be h2 db.

---
+++ src/main/java/com/example/CsvFileUploader/CsvFileUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.CsvFileUploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvFileUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvFileUploaderApplication.class, args);
+	}
+
+}
